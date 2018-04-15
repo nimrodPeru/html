7aea341a5e3fa9d50a6e3c3734397d415d049e91
nimrodPeru <nimrod2410@gmail.com>
2018-04-14 19:04:19 -0500

    capeta views
    necesario para ejs

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ var express = require("express");
 var app = express();
 var port = process.env.PORT || 3000;
 
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
 app.get("/",function(req,res){
     res.render("index.ejs")
 });
